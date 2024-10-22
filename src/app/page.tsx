509ab902eef46e6a3a799c1ce053701f97aa284d
Mason19Kelley <mason19kelley@gmail.com>
2024-10-22 00:42:15 -0500

    added nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-
+import {
+  ListItem,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { GearIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="w-screen h-screen">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/challenges" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Challenges
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="ml-auto" >
+            <NavigationMenuTrigger >
+                <GearIcon className="h-6 w-6" />
+            </NavigationMenuTrigger>
+            <NavigationMenuContent style={{ transition:'0s 180s'}}>
+              <ul className="flex flex-col gap-3 p-4 md:w-[100px] lg:w-[100px] t-9 relative">
+                <ListItem>Dark Mode</ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }
